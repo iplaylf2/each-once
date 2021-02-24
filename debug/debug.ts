7173a1794b5633372fe34ce50d7d491df855ed43
i-play-lf2 <914637118@qq.com>
2021-02-24 18:30:45 +0800

    use benchmark

--- debug/debug.ts
+++ debug/debug.ts
@@ -1,54 +1,37 @@
 import { combine, reduce, map } from "each-once";
-import { performance } from "perf_hooks";
+import { Suite } from "benchmark";
 
-const world = function (arr_size: number, repeat: number) {
+const world = function (arr_size: number) {
+  console.log(`测试数组长度为：${arr_size}`);
   const data = new Array(arr_size).fill(0);
 
   const mf = (x: number) => x;
   const rf = (r: number, x: number) => r + x;
 
-  const suit1 = function () {
-    data.map(mf).map(mf).map(mf).map(mf).map(mf).reduce(rf, 0);
-  };
-
-  const suit2 = function () {
-    const tf = combine(map(mf), map(mf), map(mf), map(mf), map(mf));
-    reduce(tf, rf, 0)(data);
-  };
-
   const tf_ = combine(map(mf), map(mf), map(mf), map(mf), map(mf));
   const reduce_ = reduce(tf_, rf, 0);
-  const suit3 = function () {
-    reduce_(data);
-  };
 
-  let t1 = 0,
-    t2 = 0,
-    t3 = 0;
-  for (let count = 0; count < repeat; count++) {
-    let t = 0;
-    let wait = 0;
-    t = performance.now();
-    suit1();
-    wait = performance.now();
-    while (performance.now() - wait < 10);
-    t1 += performance.now() - t;
-
-    t = performance.now();
-    suit2();
-    wait = performance.now();
-    while (performance.now() - wait < 10);
-    t2 += performance.now() - t;
-
-    t = performance.now();
-    suit3();
-    wait = performance.now();
-    while (performance.now() - wait < 10);
-    t3 += performance.now() - t;
-  }
-
-  console.log(t1 / repeat, t2 / repeat, t3 / repeat);
-  console.log(1, t2 / t1, t3 / t1);
+  // add tests
+  new Suite()
+    .add("原生数组", function () {
+      data.map(mf).map(mf).map(mf).map(mf).map(mf).reduce(rf, 0);
+    })
+    .add("each-once", function () {
+      const tf = combine(map(mf), map(mf), map(mf), map(mf), map(mf));
+      reduce(tf, rf, 0)(data);
+    })
+    .add("each-once 简单优化", function () {
+      reduce_(data);
+    })
+    // add listeners
+    .on("cycle", function (event: any) {
+      console.log(String(event.target));
+    })
+    .on("complete", function (this: any) {
+      console.log("Fastest is " + this.filter("fastest").map("name"));
+    })
+    // run async
+    .run({ async: true });
 };
 
-world(10000, 100);
+world(1000);
