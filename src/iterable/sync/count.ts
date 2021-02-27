6d5ab97bcf2d7276d6946d981f35bbfe46f8a604
i-play-lf2 <914637118@qq.com>
2021-02-27 16:21:42 +0800

    add count and include

---
+++ src/iterable/sync/count.ts
@@ -0,0 +1,19 @@
+import { TransduceFunction, TransduceFunctionIn } from "../../transduce/type";
+
+export function count<T extends TransduceFunction<any, any>>(tf: T) {
+  return function (iter: Iterable<TransduceFunctionIn<T>>): number {
+    let count = 0;
+    const [transduce, dispose] = tf(() => (count++, true));
+
+    for (const x of iter) {
+      const continue_ = transduce(x);
+
+      if (!continue_) {
+        return count;
+      }
+    }
+
+    dispose?.();
+    return count;
+  };
+}
---
+++ src/iterable/sync/include.ts
@@ -0,0 +1,28 @@
+import {
+  TransduceFunction,
+  TransduceFunctionIn,
+  TransduceFunctionOut,
+} from "../../transduce/type";
+
+export function include<T extends TransduceFunction<any, any>>(
+  tf: T,
+  v: TransduceFunctionOut<T>
+) {
+  return function (iter: Iterable<TransduceFunctionIn<T>>): boolean {
+    let include = false;
+    const [transduce, dispose] = tf(
+      (x) => x !== v || ((include = true), false)
+    );
+
+    for (const x of iter) {
+      const continue_ = transduce(x);
+
+      if (!continue_) {
+        return include;
+      }
+    }
+
+    dispose?.();
+    return include;
+  };
+}
