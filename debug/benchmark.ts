017a6a6c06fd5c8c57d3f4cfc761a9b35f710063
i-play-lf2 <914637118@qq.com>
2021-02-28 23:29:43 +0800

    update benchmark

--- debug/benchmark.ts
+++ debug/benchmark.ts
@@ -1,26 +1,26 @@
 import { combine, reduce, map } from "each-once";
 import { Suite } from "benchmark";
 
-const world = function (arr_size: number) {
-  console.log(`Length of test data is ${arr_size}.`);
+const world = function (map_count: number, arr_size: number) {
+  console.log(`Map ${map_count} times, length is ${arr_size}.`);
   const data = new Array(arr_size).fill(0);
 
-  const mf = (x: number) => x;
+  const mf = (x: number) => x + 1;
   const rf = (r: number, x: number) => r + x;
 
-  const tf_ = combine(map(mf), map(mf), map(mf), map(mf), map(mf));
+  const tf_ = combine(...new Array(map_count).fill(0).map(() => map(mf)));
   const reduce_ = reduce(rf, 0, tf_);
 
   // add tests
   new Suite()
-    .add("Array", function () {
-      data.map(mf).map(mf).map(mf).map(mf).map(mf).reduce(rf, 0);
+    .add("array", function () {
+      let d = data;
+      for (let i = 0; i !== map_count; i++) {
+        d = d.map(mf);
+      }
+      d.reduce(rf, 0);
     })
     .add("each-once", function () {
-      const tf = combine(map(mf), map(mf), map(mf), map(mf), map(mf));
-      reduce(rf, 0, tf)(data);
-    })
-    .add("each-once meticulously", function () {
       reduce_(data);
     })
     // add listeners
@@ -33,7 +33,11 @@ const world = function (arr_size: number) {
       this.forEach((x: any) => console.log(`x/base x ${x.hz / base}`));
     })
     // run async
-    .run({ async: true });
+    .run({ async: false });
 };
 
-world(1000);
+for (let a = 3; a <= 5; a++) {
+  for (let b = 100; b <= 100000; b *= 10) {
+    world(a, b);
+  }
+}
